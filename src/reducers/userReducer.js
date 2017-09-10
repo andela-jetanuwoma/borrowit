75e085e83bfe28a5216c3731fbab6a7b0bca5120
andela-jetanuwoma <jude.etanuwoma@andela.com>
2017-09-10 12:25:43 +0100

    Update userReducer.js
    fix late merge conflicts

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -11,11 +11,7 @@ export default function userReducer(state = initialState.auth, action) {
         isAuthenticated: !isEmpty(action.user),
         error: {},
       };
-<<<<<<< HEAD
 
-
-=======
->>>>>>> da26a38693af474de647a60114a03b0b087af309
     default:
       return state;
   }
